63c388b80e31e0572c05ec2d30453f509901d51a
user <kutsanyuri@gmail.com>
2026-03-26 20:42:51 +0200

    Exercise

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 console.log("Hello World");
 console.log("Hello");
+
+const salaryRateHourUsd = 80;
+const availableHours = (5 * 5) +  (4 * 5);
+const projectHours = 40;
+const costOfWorks = salaryRateHourUsd * availableHours;
+
+console.log('Can I work?: ' + (availableHours > projectHours));
+console.log('Cost of works: ' + costOfWorks + '$');
